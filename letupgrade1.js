667480562a05b800cd3b2f18545d564fc6201e12
priti singh <75426811+pritisingh196@users.noreply.github.com>
2023-07-22 20:28:12 +0530

    Add files via upload
    Write a JavaScript function called "calculateArea" that takes two parameters: "length" and "width".
    The function should calculate and return the area of a rectangle using the given length and width.
    function calculateArea(length, width)

---
+++ letupgrade1.js
@@ -0,0 +1,10 @@
+//Write a JavaScript function called "calculateArea" that takes two parameters: "length" and "width".
+// The function should calculate and return the area of a rectangle using the given length and width.
+function calculateArea(length, width) {
+    return length * width;
+  }
+  const length = 5;
+  const width = 3;
+  const area = calculateArea(length, width);
+  console.log("Area of the rectangle:", area); // Output: Area of the rectangle: 15
+    
